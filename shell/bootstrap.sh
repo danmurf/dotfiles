d8fc820667c67beeb0e27230bb88a46bb704b3ec
Dan Murfitt <dan@murfitt.net>
2018-12-16 20:32:12 +0000

    Add composer details to PATH and create COMPOSER_HOME environment variable.

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -2,3 +2,9 @@
 
 # Allow composer executables to be run from the project directory
 export PATH="vendor/bin:$PATH"
+
+# Add global composer executable path
+export PATH="~/.composer/vendor/bin:$PATH"
+
+# Default global composer directory
+export COMPOSER_HOME="~/.composer"
