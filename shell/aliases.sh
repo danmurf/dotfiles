0157b14391c3f9f562a03458030ea652b6541d96
Dan Murfitt <dan@murfitt.net>
2019-04-29 12:41:48 +0100

    Create alias for spinning up new docker-machine using digital ocean with preset environment variables

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,3 +2,6 @@
 
 alias ..="cd .."
 alias ll='ls -lG'
+
+# Docker machine
+alias dmdo='docker-machine create --driver digitalocean --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN --digitalocean-ssh-key-fingerprint $SSH_KEY_FINGERPRINT --digitalocean-region $DIGITALOCEAN_REGION --digitalocean-size $DIGITALOCEAN_SIZE --digitalocean-monitoring=$DIGITALOCEAN_MONITORING --digitalocean-ssh-user $DIGITALOCEAN_SSH_USER'
