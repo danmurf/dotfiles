d2caa4c9bf84d534eb4747ad30e80f23eed1db08
Dan Murfitt <dan@murfitt.net>
2020-05-21 08:15:43 +0100

    Use livereload with jekyll alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,4 +7,4 @@ alias ll='ls -lG'
 alias dmdo='docker-machine create --driver digitalocean --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN --digitalocean-ssh-key-fingerprint $SSH_KEY_FINGERPRINT --digitalocean-region $DIGITALOCEAN_REGION --digitalocean-size $DIGITALOCEAN_SIZE --digitalocean-monitoring=$DIGITALOCEAN_MONITORING --digitalocean-ssh-user $DIGITALOCEAN_SSH_USER'
 
 # Jekyll
-alias js='bundle exec jekyll serve'
+alias js='bundle exec jekyll serve --livereload'
